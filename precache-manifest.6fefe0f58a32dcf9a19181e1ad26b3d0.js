5313466e4a4a57190a84df2b3853d0c8773956cf
Roman Kirilenko <cap.spouk@gmail.com>
2019-07-22 15:57:53 +0300

    Updates

---
+++ precache-manifest.6fefe0f58a32dcf9a19181e1ad26b3d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c2c7029a764d0f8fc067cfe1576721",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "304b8d53bfdae1d26ab5",
+    "url": "/todo-list/static/css/main.98f804c6.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/todo-list/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "304b8d53bfdae1d26ab5",
+    "url": "/todo-list/static/js/main.57046231.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
